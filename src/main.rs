6afbb7bc6123342cb4bb2d97e8849bf3b8753ed4
Quentin Rasmont <qrasmont@gmail.com>
2022-05-06 23:01:38 +0200

    Basic stack replay from logs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
-fn main() {
-    println!("I say we take off and nuke the site from orbit.");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader, stdout};
+use std::{thread, time};
+use crossterm::{ExecutableCommand, QueueableCommand, terminal, cursor};
 
+fn main() -> io::Result<()> {
+    let file = File::open("test.log")?;
+    let reader = BufReader::new(file);
+    let mut stdout = stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    for read in reader.lines() {
+        if let Ok(line) = read {
+            if line.contains("+ ") || line.contains("~ "){
+
+                stdout.queue(cursor::SavePosition).unwrap();
+                stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+                let split = line.split(" -> ");
+
+                for (i, name) in split.enumerate() {
+
+                    let tabs = "\t".repeat(i);
+                    stdout.write(format!("{}{}\n", tabs, name).as_bytes()).unwrap();
+                }
+
+                stdout.queue(cursor::RestorePosition).unwrap();
+                stdout.flush().unwrap();
+
+                thread::sleep(time::Duration::from_millis(500));
+            }
+        }
+    }
+
+    Ok(())
 }
